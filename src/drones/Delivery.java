dcb2f61c347de370bafea25588255714ba88b375
Mats Högberg <mats@hgbrg.se>
2016-02-11 21:17:43 +0100

    Parsing of input

--- src/drones/Delivery.java
+++ src/drones/Delivery.java
@@ -14,9 +14,45 @@ public class Delivery {
         try {
             List<String> lines = Files.readAllLines(Paths.get("files/busy_day.in"));
 
-            System.out.println(Arrays.toString(lines.get(0).split(" ")));
+            int line = 0;
 
-            System.out.println(lines);
+            int rows = Integer.parseInt(lines.get(line).split(" ")[0]);
+            int cols = Integer.parseInt(lines.get(line).split(" ")[1]);
+            int numberOfDrones = Integer.parseInt(lines.get(line).split(" ")[2]);
+            int deadline = Integer.parseInt(lines.get(line).split(" ")[3]);
+            int maxLoad = Integer.parseInt(lines.get(line).split(" ")[4]);
+
+            line += 1;
+
+            int numberOfProductTypes = Integer.parseInt(lines.get(line));
+            int[] weights = Arrays.stream(lines.get(line + 1).split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            line += 2;
+
+            int numberOfWarehouses = Integer.parseInt(lines.get(line));
+
+            line += 1;
+
+            for (int i = 0; i < numberOfWarehouses; i++) {
+                int x = Integer.parseInt(lines.get(line).split(" ")[0]);
+                int y = Integer.parseInt(lines.get(line).split(" ")[1]);
+                int[] warehouseStock = Arrays.stream(lines.get(line + 1).split(" ")).mapToInt(Integer::parseInt).toArray();
+
+                line += 2;
+            }
+
+            int numberOfOrders = Integer.parseInt(lines.get(line));
+
+            line += 1;
+
+            for (int i = 0; i < numberOfOrders; i++) {
+                int x = Integer.parseInt(lines.get(line).split(" ")[0]);
+                int y = Integer.parseInt(lines.get(line).split(" ")[1]);
+                int numberOfItems = Integer.parseInt(lines.get(line + 1));
+                int[] items = Arrays.stream(lines.get(line + 2).split(" ")).mapToInt(Integer::parseInt).toArray();
+
+                line += 3;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
