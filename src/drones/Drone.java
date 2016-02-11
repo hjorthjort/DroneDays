3dab808a83f1cccd86ad74503868d470787e168c
hjorthjort <rikard.hjort@gmail.com>
2016-02-11 21:20:41 +0100

    Drone class

---
+++ src/drones/Drone.java
@@ -0,0 +1,12 @@
+package drones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hjorthjort
+ */
+public class Drone {
+    static int capacity;
+    List<Order> currentOrders = new ArrayList<>();
+}
---
+++ src/drones/Globals.java
@@ -0,0 +1,13 @@
+package drones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hjorthjort
+ */
+public class Globals {
+    static List<Warehouse> whs = new ArrayList<>();
+    static List<Order> orders = new ArrayList<>();
+    static List<Drone> drones = new ArrayList<>();
+}
