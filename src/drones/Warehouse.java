705870a6cab1734c7adef7feef69caf13aee1b6c
Mats Högberg <mats@hgbrg.se>
2016-02-11 22:15:44 +0100

    methods.

--- src/drones/Warehouse.java
+++ src/drones/Warehouse.java
@@ -19,4 +19,15 @@ public class Warehouse {
     public void addItem(int itemNbr, int quantity) {
         items.put(itemNbr, quantity);
     }
+
+    public boolean has(int item) {
+        return items.containsKey(item);
+    }
+
+    public void take(int item) {
+        items.put(item, items.get(item) - 1);
+        if (items.get(item) == 0) {
+            items.remove(item);
+        }
+    }
 }
